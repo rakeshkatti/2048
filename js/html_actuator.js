dc98f2d50c2600078891ef8e6373c8dc6fa37fd8
Rakesh Katti <rakesh.katti@gmail.com>
2014-04-12 20:56:45 +0530

    Tweets fixed

--- js/html_actuator.js
+++ js/html_actuator.js
@@ -155,13 +155,13 @@ HTMLActuator.prototype.scoreTweetButton = function () {
   var tweet = document.createElement("a");
   tweet.classList.add("twitter-share-button");
   tweet.setAttribute("href", "https://twitter.com/share");
-  tweet.setAttribute("data-via", "gabrielecirulli");
-  tweet.setAttribute("data-url", "http://git.io/2048");
-  tweet.setAttribute("data-counturl", "http://gabrielecirulli.github.io/2048/");
+  tweet.setAttribute("data-via", "rakesh_katti");
+  tweet.setAttribute("data-url", "http://rakeshkatti.github.io/2048/");
+  tweet.setAttribute("data-counturl", "http://rakeshkatti.github.io/2048/");
   tweet.textContent = "Tweet";
 
-  var text = "I scored " + this.score + " points at 2048, a game where you " +
-             "join numbers to score high! #2048game";
+  var text = "I scored " + this.score + " points in 2048rev, " +
+             "#2048rev #2048game";
   tweet.setAttribute("data-text", text);
 
   return tweet;
